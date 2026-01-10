8b12be839ff11a4810681f9303df0531db815c94
leilei1214 <a0933252747@gmail.com>
2026-01-10 12:59:24 +0800

    backup YYYY-MM-DD

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,7 @@
     <!-- Google Fonts -->
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Kanit:300,400,500,500i,600,900%7CRoboto:400,900">
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         .ie-panel { display: none; background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index: 1; }
         html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel { display: block; }
