b7eb04b830e6ab608d11a7638fc62775405e4d48
leilei1214 <a0933252747@gmail.com>
2026-01-10 10:03:48 +0800

    backup YYYY-MM-DD

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/hello', function () {
     return 'Hello Laravel 👋';
 });
+
 Route::get('/', [PageController::class, 'home']);
 
