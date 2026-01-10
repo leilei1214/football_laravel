e727e433ab0c390901dc49494fec19f36e01973d
leilei1214 <a0933252747@gmail.com>
2026-01-10 12:43:49 +0800

    backup YYYY-MM-DD

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,22 +1,50 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="wide wow-animation" lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', '我的網站')</title>
+    <title>@yield('title', 'Home')</title>
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
 
     <!-- Google Fonts -->
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Kanit:300,400,500,500i,600,900%7CRoboto:400,900">
-    
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Kanit:300,400,500,500i,600,900%7CRoboto:400,900">
+
     <!-- CSS -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    
+
     <style>
-        .ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} 
-        html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}
+        .ie-panel { display: none; background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index: 1; }
+        html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel { display: block; }
+
+        .object-container {
+            width: 100%;
+            max-width: 1212px;
+            height: 200px;
+            overflow: hidden;
+            border: 2px solid #007BFF;
+            border-radius: 8px;
+            box-shadow: 0 4px 10px rgba(0,0,0,0.1);
+            background-color: #ffffff;
+            margin-bottom: 20px;
+        }
+
+        object {
+            width: 100%;
+            height: 100%;
+            border: none;
+            overflow: hidden !important;
+        }
+
+        .object-container::-webkit-scrollbar {
+            display: none;
+        }
     </style>
+
+    @stack('styles')
 </head>
 <body>
     
