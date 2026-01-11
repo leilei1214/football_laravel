b42f682a841c2455bdeb188f7112aa7372b74c48
leilei1214 <a0933252747@gmail.com>
2026-01-11 13:13:33 +0800

    Update app blade and add Swiper JS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,12 +61,11 @@
 
     <!-- JS -->
     <!-- <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 
     <script src="{{ asset('js/script.js') }}"></script>
     <script src="{{ asset('js/rd-navbar.min.js') }}"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
     <script src="{{ asset('js/core.min.js') }}"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/regula.js"></script>
