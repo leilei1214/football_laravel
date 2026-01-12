518011f6bfbd4e5b9af0dc0592df1eeae1ac0842
leilei1214 <a0933252747@gmail.com>
2026-01-13 01:57:44 +0800

    Update app blade and add Swiper JS

--- public/js/script.js
+++ public/js/script.js
@@ -270,4 +270,10 @@ document.write('<meta name="viewport" content="width=device-width,initial-scale=
 function event_content_href(id){
 	window.location.href = `./event_content?list_id=${id}`
 }
+document.querySelectorAll('[data-custom-toggle]').forEach(btn => {
+    btn.addEventListener('click', () => {
+        const target = document.querySelector(btn.dataset.customToggle);
+        target.classList.toggle('active');
+    });
+});
 
