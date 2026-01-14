56fa4bdf75c13042ece02f7a2664ead497cb91a3
leilei1214 <a0933252747@gmail.com>
2026-01-15 01:55:40 +0800

    Update app blade and add Swiper JS

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,8 @@
 <?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
