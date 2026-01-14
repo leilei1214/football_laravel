e68d1a84b556b41fe1d4991f334f7efa46c0c216
leilei1214 <a0933252747@gmail.com>
2026-01-15 02:24:17 +0800

    Update app blade and add Swiper JS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,7 +59,9 @@
 
     <main>
         @yield('content')
+
     </main>
+     @yield('scripts') <!-- <<--- 一定要有 -->
     <script src="https://cms.devoffice.com/coding-dev/rd-navbar/demo/js/core.min.js"></script>
     <script src="https://cms.devoffice.com/coding-dev/rd-navbar/demo/js/script.js"></script>
     <script src="https://cms.devoffice.com/coding-dev/rd-navbar/demo/js/smoothscroll.min.js"></script>
