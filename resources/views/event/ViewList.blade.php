557c3a7ae88896319c5ba80646429df5ad4c0598
leilei1214 <a0933252747@gmail.com>
2026-01-18 15:02:39 +0800

    Update app blade and add Swiper JS

--- resources/views/event/ViewList.blade.php
+++ resources/views/event/ViewList.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.app')
 
-@section('title', 'Login Page')
-
+@section('title', 'EventViewList')
+<!-- @section('style')
+.nav .nav-tabse {
+    background: #fff;
+    border: 1px solid #e1e1e1;
+    /* width: auto; */
+    display: -webkit-inline-box;
+}
+@endsection -->
 @section('content')
 <section class="section section-variant-1 bg-gray-100">
     <div class="container">
@@ -34,12 +41,15 @@
                     </div>
                     </div>
                 </article>
-                <ul class="nav nav-tabs" role="tablist">
-                    <li class="nav-item" role="presentation"><a class="nav-link active show" href="#tabs-modern-1" data-bs-toggle="tab" aria-selected="true" role="tab">總覽</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link active show" href="#tabs-modern-1" data-bs-toggle="tab" aria-selected="true" role="tab">基礎</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-modern-2" data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">樂踢</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-modern-3" data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">實戰</a></li>
-                </ul>
+                <div id="tabs-modern">
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li class="nav-item" role="presentation"><a class="nav-link active show" href="#tabs-modern-1" data-bs-toggle="tab" aria-selected="true" role="tab">總覽</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link active show" href="#tabs-modern-1" data-bs-toggle="tab" aria-selected="true" role="tab">基礎</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-modern-2" data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">樂踢</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="#tabs-modern-3" data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">實戰</a></li>
+                    </ul>                    
+                </div>
+
                 <!-- Owl Carousel-->
                 <div class="owl-carousel owl-spacing-1 owl-loaded" data-items="3" data-dots="false" data-nav="true" data-autoplay="true" data-autoplay-speed="4000" data-stage-padding="0" data-loop="false" data-margin="30" id="myCarousel"  data-mouse-drag="false" data-nav-custom=".owl-carousel-outer-navigation">
                     
