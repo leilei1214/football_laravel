00a8682e70445e723c4a0a83f6e5f78d332d3ef7
leilei1214 <a0933252747@gmail.com>
2026-01-21 21:03:52 +0800

    Update app blade and add Swiper JS

--- app/Http/Controllers/event/EventController.php
+++ app/Http/Controllers/event/EventController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\event;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class EventController extends Controller
 {
      public function ShowEvent()
