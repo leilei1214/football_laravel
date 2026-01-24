31ce2b729b1a73ce178f08e07ced67aa7d731464
leilei1214 <a0933252747@gmail.com>
2026-01-24 15:53:48 +0800

    Update app blade and add Swiper JS

--- resources/views/Manager/EventList.blade.php
+++ resources/views/Manager/EventList.blade.php
@@ -42,7 +42,7 @@
 @section('scripts')
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script> -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src = "http://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js" defer></script>
  
