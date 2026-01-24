9862ac5d2db98c14d61561445fd175eb6997048b
leilei1214 <a0933252747@gmail.com>
2026-01-24 10:17:05 +0800

    Update app blade and add Swiper JS

--- resources/views/event/AddEvent.blade.php
+++ resources/views/event/AddEvent.blade.php
@@ -121,7 +121,7 @@
 
 @section('content')
 <section class="section section-md bg-gray-100">
-<div class="col-8" style="margin:0 auto">
+<div class="col-12 col-md-8 " style="margin:0 auto">
     <div class="card index_page_edit">
 
         <div class="card-body" style="background-color: #fff;border-radius: 0px 0px 16px 16px;">
