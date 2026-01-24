49c072c02a7179db3f24140d51be80e1fb65218d
leilei1214 <a0933252747@gmail.com>
2026-01-24 15:25:01 +0800

    Update app blade and add Swiper JS

--- resources/views/Manager/EventList.blade.php
+++ resources/views/Manager/EventList.blade.php
@@ -2,6 +2,8 @@
 
 @section('title', 'EventViewList')
 @section('style')
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+
 <link rel="stylesheet" href="{{ asset('css/event/style.css') }}">
 <style>
 .product-header .badge {
@@ -37,7 +39,7 @@
 @endsection
 @section('scripts')
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
 <!-- <script src="{{ asset('js/event_level.js') }}"></script> -->
 <script>
@@ -66,10 +68,8 @@ fetch('/check-identity')
 <!-- <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script> -->
 
 <!-- 2️⃣ DataTables CSS -->
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
 
 <!-- 3️⃣ DataTables JS -->
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script>
     //   fetch('./api/event', {
     //       method: 'POST',
