30f840a0b933a7fe3c36c0dca219a73183f4912b
leilei1214 <a0933252747@gmail.com>
2026-01-24 15:49:03 +0800

    Update app blade and add Swiper JS

--- resources/views/Manager/EventList.blade.php
+++ resources/views/Manager/EventList.blade.php
@@ -2,7 +2,6 @@
 
 @section('title', 'EventViewList')
 @section('style')
-<link rel="stylesheet" href="https://cdn.datatables.net">
 
 <link rel="stylesheet" href="{{ asset('css/event/style.css') }}">
 <style>
@@ -38,11 +37,10 @@
 
 @endsection
 @section('scripts')
-<script src="https://code.jquery.com"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="https://cdn.datatables.net"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
 <!-- <script src="{{ asset('js/event_level.js') }}"></script> -->
 <script>
