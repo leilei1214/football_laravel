2377d70737aa2552ca5f917d388a3f48ef83a414
leilei1214 <a0933252747@gmail.com>
2026-02-08 23:42:38 +0800

    Update app blade and add Swiper JS

--- resources/views/Manager/SignIn.blade.php
+++ resources/views/Manager/SignIn.blade.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
 
-
+<link rel="stylesheet" href="{{ asset('css/event/style.css') }}">
 <link rel="stylesheet" href="{{ asset('css/Manager.css') }}">
 <style>
 .product-header .badge {
