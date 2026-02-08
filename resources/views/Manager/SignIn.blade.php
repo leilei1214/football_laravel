ca98d94cbf62d270b2d0fe888604904bed2c5f94
leilei1214 <a0933252747@gmail.com>
2026-02-08 22:21:31 +0800

    Update app blade and add Swiper JS

---
+++ resources/views/Manager/SignIn.blade.php
@@ -0,0 +1,463 @@
+@extends('layouts.app')
+
+@section('title', 'EventViewList')
+@section('style')
+
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+
+
+<link rel="stylesheet" href="{{ asset('css/Manager.css') }}">
+<style>
+.product-header .badge {
+    position: relative;
+    font-size: 20px;
+    padding: 10px;
+}
+</style>
+@endsection
+
+
+@section('content')
+<section class="section section-md bg-gray-100">
+    <div class="container">
+        <div class="row row-50">
+            <div class="col-lg-8 ">
+
+
+
+                <div class="blog-post" style="margin: -4px;">
+                <!-- Badge-->
+                    <div id="activity_level">
+
+                    </div>
+                <!-- <div class="badge badge-secondary">The Team
+                </div> -->
+                <!-- <div class="badge badge-secondary">The Team
+                </div> -->
+                <h3 class="blog-post-title"></h3>
+                <div class="blog-post-header">
+                    <div class="blog-post-author">
+                    <img class="img-circle" src="/images/logo.png" alt="" width="63" height="63">
+
+                    <!-- <img class="img-circle" src="images/user-3-63x63.jpg" alt="" width="63" height="63"> -->
+                    <p class="post-author organizer"></p>
+                    </div>
+                    <div class="blog-post-meta">
+                    <time class="blog-post-time" datetime="2024"><span class="icon mdi mdi-clock"></span><span class="active_date"></span></time>
+                    <div class="blog-post-comment"><span class="icon mdi mdi-map-marker"></span><span class="location"></span></div>
+                    <div class="blog-post-view"><span class="icon mdi mdi-account-multiple"></span><span class="current_participants"></span>/<span class="max_participants"></span> </div>
+                    <div class="blog-post-view "><span class="icon">$</span><span class="icon amount"></div>
+                    <div class="blog-post-view Boss_edit"><span class="icon">應收:</span><span class="icon sum_amount"></div>
+
+
+                    </div>
+                </div>
+                <div class="blog-post-author-quote">
+                    <p class="activity_intro"></p>
+                </div>
+
+                </div>
+                <div class="row">
+                    <!-- mx-auto -->
+                    <ul class="list-inline mx-auto list-inline-xs">
+                        <li><a class="button button-md button-primary btn_add" href="#">編輯</a></li>
+                        <li><a class="button button-md button-primary btn_Nadd" href="#">刪除</a></li>
+
+                    </ul>
+                    <ul class="list-inline mx-auto list-inline-xs">
+                        <li><a class="button button-md button-primary btn_add" href="#" onclick="ClockOut()">簽到表</a></li>
+                        <li><a class="button button-md button-primary btn_Nadd" href="#" onclick="QrcodeSign()">Qrcode簽到</a></li>
+
+                    </ul>
+
+
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 owl-carousel-outer-navigation">
+                    <article class="heading-component" >
+                        <div class="heading-component-inner">
+                        <h5 class="heading-component-title">錄取名單
+                        </h5>
+                        </div>
+                    </article>
+                    </div>
+                    <div class="table-custom-responsive">
+                        <table class="table">
+                        <!-- <div class="badge badge-secondary">錄取名單
+                        </div> -->
+                        <thead class="table-standings ">
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">名字</th>
+                            <th scope="col">位置</th>
+                            <th scope="col">簽到</th>
+                            <th scope="col">簽退</th>
+                            <th scope="col">繳費</th>
+
+                            </tr>
+                        </thead>
+                        <tbody id="add_registrations" style="background-color: #ffffff;">
+                            <!-- <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                            </tr> -->
+                        </tbody>
+                        </table>
+                    </div>
+                    <div class="d-block w-100 text-center mx-auto" onclick="save()">
+                        <a class="button-md icon-media icon-media-round icon-media-instagram " href="#">儲存</a>
+
+                    </div>
+                </div>
+
+                
+            </div>
+
+        </div>
+    </div>
+</section>
+@endsection
+@section('scripts')
+
+<script sr 淤c="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src = "https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js" ></script>
+ 
+
+<script>
+    // 修正你的 fetch 語法錯誤 (少了一個括號)
+    fetch('/check-identity')
+      .then(res => res.json())
+      .then(data => {
+        console.log('Level:', data.level, 'Guild:', data.Guild);
+        if(data.level == 1){
+            $(".addEventHref").addClass("d-none");
+        }
+      })
+      .catch(err => console.error('Fetch error:', err));
+</script>
+<script>
+    async function loadEventContent() {
+        const params = new URLSearchParams(window.location.search);
+        const listId = params.get('list_id');
+        const guildId = params.get('guild_id');
+
+        if (!listId || !guildId) {
+            alert("連結錯誤");
+            setTimeout(() => {
+                window.location.href = "/";
+            }, 1000);
+            return;
+        }
+
+        try {
+            const response = await fetch('/api/event/content', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': document
+                        .querySelector('meta[name="csrf-token"]')
+                        ?.getAttribute('content')
+                },
+                body: JSON.stringify({
+                    list_id: listId,
+                    guild_id: guildId
+                })
+            });
+
+            if (!response.ok) {
+                alert('讀取失敗');
+                return;
+            }
+            if (response.ok) {
+                const res = await response.json();
+
+                const data = res.event
+                console.log(data)
+                // 遍歷陣列並輸出每個元素
+                // for (const item of data.activity_category) {
+                //   document.getElementById('activity_level').innerHTML += `
+                //     <div class="badge badge-secondary">${item}
+                //     </div>
+                //   `
+                // }
+                const levels = data.activity_level.replace(/^{|}$/g, '').split(',');
+
+                for (const item of levels) {
+                    if( item == "基礎"){
+                    document.getElementById('activity_level').innerHTML += `
+                        <div class="badge badge-primary">${item}
+                        </div>
+                    `
+                    }
+                    else if( item == "實踐"){
+                    document.getElementById('activity_level').innerHTML += `
+                        <div class="badge badge-red">${item}
+                        </div>
+                    `
+                    }
+                    else{
+                    document.getElementById('activity_level').innerHTML += `
+                        <div class="badge badge-secondary">${item}
+                        </div>
+                    `
+                    }
+
+                }
+                $(".blog-post-title").html(data.activity_notice)
+                // const taiwanTime = new Date(data.time).toLocaleString('zh-TW', { timeZone: 'Asia/Taipei', hour12: false });
+
+                const taiwanDate = new Date(data.time);
+                taiwanDate.setHours(taiwanDate.getHours()-7 );
+                // const taiwanDate =  new Date(new Date(date.getTime() + 8 * 60 * 60 * 1000));
+                // 提取年月日和小時分鐘
+                const year = taiwanDate.getFullYear();
+
+                const month = String(taiwanDate.getMonth() + 1).padStart(2, '0'); // 月份從0開始，需要+1
+                const day = String(taiwanDate.getDate()).padStart(2, '0');
+                const hours = String(taiwanDate.getHours()).padStart(2, '0');
+                const minutes = String(taiwanDate.getMinutes()).padStart(2, '0');
+
+                // 組合成所需格式
+                const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}`;
+                // console.log(formattedDate); // "2024-11-01 06:00"
+                $(".amount").html(data.amount)
+                $(".current_participants").html(data.current_participants)
+                $(".max_participants").html(data.max_participants)
+                $(".location").html(data.location)
+                $(".organizer").html(data.organizer)
+                $(".active_date").html(formattedDate)
+                $(".activity_intro").html(data.activity_intro)
+                $(".sum_amount").html(data.amount*data.current_participants)
+
+                localStorage.setItem('Guild',data.club);
+                registrations.forEach((registration, index) => {
+                    let check_in = `
+                    <label class="checkbox-inline input-filter input-filter-danube">
+                        <input name="input-checkbox-6" id="check_in-${registration.identifier}" value="${registration.identifier}" type="checkbox" class="checkbox-custom SignIn"><span class="checkbox-custom-dummy"></span>
+                    </label>
+                    `;
+
+
+                    let check_out = `
+                        <label class="checkbox-inline input-filter input-filter-geraldine">
+                        <input name="input-checkbox-9" id="check_out-${registration.identifier}" value="${registration.identifier}" type="checkbox" class="checkbox-custom SignOut"><span class="checkbox-custom-dummy"></span>
+                    </label>
+
+                    `; // Corrected this line
+
+
+                    let payment_status = `
+                    <label class="checkbox-inline input-filter input-filter-blue">
+                        <input name="input-checkbox-9" id="payment_status-${registration.identifier}" value="${registration.identifier}" type="checkbox" class="checkbox-custom SignFree"><span class="checkbox-custom-dummy"></span>
+                    </label>
+                    `;
+
+
+                    console.log(`Registration ${index + 1}:`, registration);
+
+                    add_registrations += `
+                        <tr>
+                        <th scope="row">${index + 1}</th>
+                        <td>${registration.username}</td>
+                        <td>${registration.preferred_position1}、${registration.preferred_position2}</td>
+                        <td>${check_in}</td>
+                        <td>${check_out}</td>
+                        <td>${payment_status}</td>
+                        </tr>
+                    `;
+                    document.getElementById("add_registrations").innerHTML += add_registrations
+                    if(registration.check_in == 1){
+
+                        const specificCheckbox = document.getElementById(`check_in-${registration.identifier}`);
+                        if (specificCheckbox) {
+                        specificCheckbox.checked = true;
+                        console.log(`Checkbox with id unique-checkbox-${registration.identifier} is checked.`);
+                        }
+                    }
+                    if (registration.check_out == 1) {
+                        const specificCheckbox = document.getElementById(`check_out-${registration.identifier}`);
+                        if (specificCheckbox) {
+                        specificCheckbox.checked = true;
+                        console.log(`Checkbox with id unique-checkbox-${registration.identifier} is checked.`);
+                        }
+                        // check_out = "已簽退";
+                    }
+                    if (registration.payment_status) {
+                        // payment_status = registration.payment_status; // Use the actual name
+                        const specificCheckbox = document.getElementById(`payment_status-${registration.identifier}`);
+                        if (specificCheckbox) {
+                        specificCheckbox.checked = true;
+                        console.log(`Checkbox with id unique-checkbox-${registration.identifier} is checked.`);
+                        }
+                    }
+                    add_registrations = ``
+                });
+
+
+            } 
+
+
+        } catch (err) {
+            console.error(err);
+            alert('系統錯誤');
+        }
+    }
+
+    // 頁面載入後執行
+    document.addEventListener('DOMContentLoaded', loadEventContent);
+</script>
+<script>
+      function ClockOut() {
+        const currentUrl = window.location.href;
+
+        // 使用 URLSearchParams 提取查詢參數
+        const urlParams = new URLSearchParams(window.location.search);
+
+        // 獲取 `list_id` 的值
+        const activityId = urlParams.get('list_id');
+
+        window.location.href ="./SignIn?list_id="+activityId
+
+      }
+      function QrcodeSign(){
+        const currentUrl = window.location.href;
+
+        // 使用 URLSearchParams 提取查詢參數
+        const urlParams = new URLSearchParams(window.location.search);
+
+        // 獲取 `list_id` 的值
+        const activityId = urlParams.get('list_id');
+        const Guild = localStorage.getItem('Guild');
+
+        window.location.href ="./Sign_Qrcode?list_id="+activityId+"&Guild="+Guild
+
+      }
+</script>
+
+<script>
+    function save(){
+    const currentUrl = window.location.href;
+
+    // 使用 URLSearchParams 提取查詢參數
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // 獲取 `list_id` 的值
+    const activityId = urlParams.get('list_id');
+    // SignIn SignOut SignFree
+    let jsonData = []; // 用于存储复选框信息的数组
+
+    // 遍历所有 .SignIn 复选框
+    $(".SignIn").each(function () {
+        let isChecked = $(this).is(":checked"); // 检查是否选中
+        let value = $(this).val(); // 获取复选框的值
+
+        // 将状态和值存入 JSON 数组
+        jsonData.push({
+            checked: isChecked,  // 是否选中
+            value: value,      // 复选框的值
+            class: "SignIn",
+            time: new Date()
+        });
+    });
+            // 遍历所有 .SignIn 复选框
+    $(".SignOut").each(function () {
+        let isChecked = $(this).is(":checked"); // 检查是否选中
+        let value = $(this).val(); // 获取复选框的值
+
+        // 将状态和值存入 JSON 数组
+        jsonData.push({
+            checked: isChecked,  // 是否选中
+            value: value,         // 复选框的值
+            class: "SignOut",
+            time: new Date()
+
+        });
+    });
+            // 遍历所有 .SignIn 复选框
+    $(".SignFree").each(function () {
+        let isChecked = $(this).is(":checked"); // 检查是否选中
+        let value = $(this).val(); // 获取复选框的值
+
+        // 将状态和值存入 JSON 数组
+        jsonData.push({
+            checked: isChecked,  // 是否选中
+            value: value,         // 复选框的值
+            class:"SignFree",
+            time: new Date()
+            
+        });
+    });
+    // 打印结果
+    console.log(jsonData);
+    fetch('./Update_SignIn', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            activityId: activityId,
+            jsonData:jsonData
+        })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status == 200) {
+                alert("儲存成功！");
+                location.reload();
+            } else {
+                alert("儲存失败：" + data.message);
+            }
+        })
+        .catch(error => {
+            console.error("请求失败:", error);
+        });
+    }
+    // $(".btn_add").click(()=>{
+    //   const currentUrl = window.location.href;
+
+    //   // 使用 URLSearchParams 提取查詢參數
+    //   const urlParams = new URLSearchParams(window.location.search);
+
+    //   // 獲取 `list_id` 的值
+    //   const activityId = urlParams.get('list_id');
+    //   fetch('./insert-event', {
+    //       method: 'POST',
+    //       headers: {
+    //           'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify({
+    //         status_add: 1,
+    //         activityId:activityId
+    //       }),
+    //   })
+    //   .then(response => response.json())
+    //   .then(data => {
+    //       if (data.status == 200) {
+    //           alert("報名成功！");
+    //       } else {
+    //           alert("報名失败：" + data.message);
+    //       }
+    //   })
+    //   .catch(error => {
+    //       console.error("请求失败:", error);
+    //   });
+    // })
+</script>
+
+@endsection
