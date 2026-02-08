58beb9a2798c0923f1371834876fd0ab2c2d6365
leilei1214 <a0933252747@gmail.com>
2026-02-08 11:11:16 +0800

    Update app blade and add Swiper JS

--- resources/views/event/USEREventContent.blade.php
+++ resources/views/event/USEREventContent.blade.php
@@ -534,6 +534,9 @@
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': document
+                        .querySelector('meta[name="csrf-token"]')
+                        ?.getAttribute('content')
                 },
                 body: JSON.stringify({
                 status_add: 1,
