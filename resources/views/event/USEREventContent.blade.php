9a544e4dd2d97fa42d0e58412180eba58025ece0
leilei1214 <a0933252747@gmail.com>
2026-02-08 11:08:18 +0800

    Update app blade and add Swiper JS

--- resources/views/event/USEREventContent.blade.php
+++ resources/views/event/USEREventContent.blade.php
@@ -226,6 +226,7 @@
 @endsection
 @section('scripts')
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
 <!-- <script src="{{ asset('js/event_level.js') }}"></script> -->
 <script>
