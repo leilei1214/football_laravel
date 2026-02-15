3c5cf7d9fc37aac96337994d118fcd382bb68834
leilei1214 <a0933252747@gmail.com>
2026-02-15 14:12:19 +0800

    Update app blade and add Swiper JS

--- public/js/myScript.js
+++ public/js/myScript.js
@@ -2,9 +2,7 @@ function getURLParameter(name) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(name);
 }
-function event_content_href(id){
-	window.location.href = `./event_content?list_id=${id}`
-}
+
 document.querySelectorAll('[data-custom-toggle]').forEach(btn => {
     btn.addEventListener('click', () => {
         const target = document.querySelector(btn.dataset.customToggle);
