e1ba168a9721d4e07ccdd4ec8b6ed6d85067621d
leilei1214 <a0933252747@gmail.com>
2026-02-23 22:23:57 +0800

    Update app blade and add Swiper JS

--- resources/views/Club/ClubList.blade.php
+++ resources/views/Club/ClubList.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'ClubViewList')
 @section('style')
 
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css">
+<!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css"> -->
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/> -->
 
 
