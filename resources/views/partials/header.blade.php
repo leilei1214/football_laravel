7bad5761a6b60900f3d6c1efd3bcb71968332431
leilei1214 <a0933252747@gmail.com>
2026-02-26 01:48:51 +0800

    Update app blade and add Swiper JS

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -97,12 +97,12 @@
                             <a class="link link-icon link-icon-left link-classic login" id="user_profile_link" href="/login?status=login&club=4&level=4">
                                 <img src="/images/default-avatar.png" alt="Avatar" 
                                     style="width: 32px; height: 32px; border-radius: 50%; object-fit: cover; vertical-align: middle; margin-right: 5px;">
-                                <span class="link-icon-text  d-none" id="employee">Your Account</span>
+                                <span class="link-icon-text" >請登入</span>
                             </a>
                             <a class="link link-icon link-icon-left link-classic logout d-none" id="user_profile_link" href="#">
                                 <img id="user_avatar" src="/images/default-avatar.png" alt="Avatar" 
                                     style="width: 32px; height: 32px; border-radius: 50%; object-fit: cover; vertical-align: middle; margin-right: 5px;">
-                                <!-- <span class="link-icon-text  d-none" id="employee">LOGIN</span> -->
+                                <span class="link-icon-text " id="employee">LOGIN</span>
                             </a>
                         </li>
                     </ul>
