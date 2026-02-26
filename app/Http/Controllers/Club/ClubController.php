9d0ace193e5dd8ccbb18dc6174054fd260af704b
leilei1214 <a0933252747@gmail.com>
2026-02-27 00:24:27 +0800

    Update app blade and add Swiper JS

--- app/Http/Controllers/Club/ClubController.php
+++ app/Http/Controllers/Club/ClubController.php
@@ -14,7 +14,68 @@
         }
         public function ClubProfile()
         {
-            return view('Club.ClubProfile');
+                    // 範例：從資料庫獲取球隊資料
+        // $team = Team::with(['players', 'stats', 'achievements'])->findOrFail($id);
+        
+        // 示範用的假資料
+            $team = [
+                'name' => '台北雄鷹隊',
+                'nameEn' => 'Taipei Eagles',
+                'logo' => 'https://images.unsplash.com/photo-1761325970487-05c2541653eb',
+                'coverImage' => 'https://images.unsplash.com/photo-1624295019913-0420eae31f22',
+                'founded' => 2015,
+                'stadium' => '台北市立體育場',
+                'capacity' => '20,000',
+                'league' => '台灣職業足球聯賽',
+                'manager' => '王建民',
+                'colors' => ['深藍色', '金色'],
+                'stats' => [
+                    'matches' => 30,
+                    'wins' => 18,
+                    'draws' => 6,
+                    'losses' => 6,
+                    'goalsFor' => 52,
+                    'goalsAgainst' => 28,
+                    'points' => 60,
+                ],
+                'players' => [
+                    [
+                        'id' => 1,
+                        'name' => '陳威廷',
+                        'number' => 10,
+                        'position' => '前鋒',
+                        'image' => 'https://images.unsplash.com/photo-1682486519525-a2c2d1c65b8b',
+                    ],
+                    [
+                        'id' => 2,
+                        'name' => '林俊傑',
+                        'number' => 7,
+                        'position' => '中場',
+                        'image' => 'https://images.unsplash.com/photo-1677119966332-8c6e9fb0efab',
+                    ],
+                    [
+                        'id' => 3,
+                        'name' => '黃志強',
+                        'number' => 9,
+                        'position' => '前鋒',
+                        'image' => 'https://images.unsplash.com/photo-1760046997065-25e4d6352bc8',
+                    ],
+                    [
+                        'id' => 4,
+                        'name' => '王美玲',
+                        'number' => 23,
+                        'position' => '後衛',
+                        'image' => 'https://images.unsplash.com/photo-1573113062125-2ccf3cbb67df',
+                    ],
+                ],
+                'achievements' => [
+                    ['year' => 2024, 'title' => '聯賽亞軍'],
+                    ['year' => 2023, 'title' => '盃賽冠軍'],
+                    ['year' => 2022, 'title' => '聯賽季軍'],
+                ],
+            ];
+
+            return view('Club.ClubProfile', compact('team'));
         }
         
         public function getGuilds(Request $request)
--- resources/views/Club/ClubProfile.blade.php
+++ resources/views/Club/ClubProfile.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', '球員資料')
+@section('title', '公會簡介')
 @section('style')
 
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css">
@@ -11,6 +11,7 @@
 <link rel="stylesheet" href="{{ asset('css/Manager.css') }}">
 <script src="https://cdn.tailwindcss.com"></script>
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
 
 <style>
 [x-cloak] { display: none !important; }
@@ -26,7 +27,6 @@
 
 @section('content')
 <section class="section section-md bg-gray-100">
-    
     <div class="min-h-screen">
         {{-- Cover Image --}}
         <div class="relative h-64 lg:h-80 overflow-hidden bg-gradient-to-b from-zinc-900 to-zinc-700">
@@ -356,11 +356,11 @@
     </div>
 </section>
 @endsection
-@section('scripts')
 
+@section('scripts')
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src = "https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js" ></script>
-
-
+<script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js" ></script>
 @endsection
+
+ 
