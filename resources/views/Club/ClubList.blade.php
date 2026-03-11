48ea9d64ff9ab775a2aa0806338274065b743a39
leilei1214 <a0933252747@gmail.com>
2026-03-12 07:36:09 +0800

    Update app blade and add Swiper JS

--- resources/views/Club/ClubList.blade.php
+++ resources/views/Club/ClubList.blade.php
@@ -230,7 +230,7 @@ ul#pagination li.active button {
     }
     function register_club(id){
         // window.location.href = `/Club/registerClub/${id}`
-        fetch('/club/register/' + id{
+        fetch('/club/register/' + id, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -239,7 +239,6 @@ ul#pagination li.active button {
                     ?.getAttribute('content')
             },
             body: JSON.stringify({ identifier: "none" })
-            // body: JSON.stringify({ identifier: "none",level:level })
         })
         .then(res => res.json())
         .then(data => {
